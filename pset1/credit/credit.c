21794675e5b2c5ca38e73721da3b8334578e6601
Adam Roe <adam.j.roe@gmail.com>
2018-09-18 05:43:19 +0000

    ugly but seemingly working

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -19,30 +19,49 @@ int main(void){
     //covert lli to string
     lli_to_string(cardnumber, card_string);
 
-    //print company
-    printCompany(card_string);
-
-
     //variable to hold sum of "even" digits
-    int sum_even = 0;
+    int sum = 0;
 
-    //TODO:
-    // make loop over string return an array of ints.
-    // double each value in the array
-    // sum digits into a single number
+    char c_dig; // hold a single digit as char
+    int i2_dig; // hold a single digit as int after being x2-ed
+
+    int i_dig; // hold a single digit as int
 
     //print if position is odd, counting from the back.
     //reverse iterate on a string in C: https://stackoverflow.com/questions/20394387/string-reverse-with-loop-and-array-in-c
+
     for (int i =  strlen(card_string); i > 0 ; i--){
         if (i%2==0){
-            printf("i:%i, c:%c \n",i,card_string[strlen(card_string)-i]);
+            // extract current character
+            c_dig = card_string[strlen(card_string)-i];
+            //char to int and multiply by 2
+            i2_dig =  (c_dig - '0')*2;
+            // debug: printf("i:%i, c:%c, int: %i, i2: %i \n",i,c_dig,c_dig-'0',i2_dig);
+
+            // get digits, clean solution: https://stackoverflow.com/questions/3118490/getting-each-individual-digit-from-a-whole-integer
+
+            int single_dig = 0;
+            while (i2_dig){
+                single_dig =  i2_dig % 10;
+                // debug: printf("%i\n",single_dig);
+                sum += single_dig;
+                i2_dig /= 10;
+            }
+        }else{
+            i_dig = (card_string[strlen(card_string)-i]) - '0';
+            //debug: printf("digit:%i\n",i_dig);
+            sum +=i_dig;
         }
-        // TODO: summing return ASCII values rn :/
-    //    else{
-    //        sum_even += card_string[strlen(card_string)-i];
-    //    }
     }
-    printf("sum even: %i", sum_even);
+    //debug: printf("sum: %i\n", sum);
+     if (sum % 10 != 0){
+         printf("INVALID\n");
+     }else{
+        //print company
+        printCompany(card_string);
+     }
+
+     return 0;
 }
 
 //covert lli to string
@@ -56,33 +75,47 @@ const char* lli_to_string(long long n, char* s){
 //ugly code to find the card company.
 void printCompany(char* s){
 
+    //variables for first and second characters
     char first = s[0];
     char second = s[1];
     int i_second = second - '0';
 
+    //length of credit card string
+    int length = strlen(s);
+    printf("length: %i\n", length);
 
     switch (first){
 
         //Visa, easy.
         case '4':
-        printf("Visa\n");
+            if (length == 13 || length == 16){
+                printf("Visa\n");
+            }
         break;
 
         //AmEx
         case '3':
             if ( second=='4' || second == '7'){
-                printf("AMEX\n");
-                break;
+                 if (length == 15){
+                    printf("AMEX\n");
+                 }
             }
+            break;
 
+        //MasterCard
         case '5':
-            for (int i=1; i<6;i++){
-                if ( i_second == i){
-                    printf("MC\n");
-                    break;
+            if (length == 16){
+                for (int i=1; i<6;i++){
+                    if ( i_second == i){
+                        printf("MASTERCARD\n");
+                        break;
+                    }
                 }
             }
+        default:
+         printf("INVALID\n");
         }
+
 
     return;
 }
